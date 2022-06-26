15fc56c3cc53efe0f6dd63bbb0cf045980b6e29a
benghaner <benghaner@gmail.com>
2022-06-26 14:57:59 -0400

    Implement first section of new add trail page

---
+++ BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazingTrails.Shared.Features.ManageTrails;
+
+public class TrailDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string Location { get; set; } = "";
+    public int TimeInMinutes { get; set; }
+    public int Length { get; set; }
+    public List<RouteInstruction> Route { get; set; } = new();
+
+    public class RouteInstruction
+    {
+        public int Stage { get; set; }
+        public string Description { get; set; } = "";
+    }
+}
