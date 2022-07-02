6a19cfd540aceeb5def18fda3102dcbabe716eb2
benghaner <benghaner@gmail.com>
2022-07-02 16:05:42 -0400

    Add validation

--- BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
+++ BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -27,6 +27,7 @@ public class TrailValidator : AbstractValidator<TrailDto>
         RuleFor(x => x.Description).NotEmpty().WithMessage("Please enter a description");
         RuleFor(x => x.Location).NotEmpty().WithMessage("Please enter a location");
         RuleFor(x => x.Length).GreaterThan(0).WithMessage("Please enter a length");
+        RuleFor(x => x.TimeInMinutes).GreaterThan(0).WithMessage("Please enter a time");
         RuleFor(x => x.Route).NotEmpty().WithMessage("Please add a route instruction");
         RuleForEach(x => x.Route).SetValidator(new RouteInstructionValidator());
     }
