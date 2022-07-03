a3a9d8dec0acee1072913f1cc68e2732d356363e
Ben Ghaner <78243998+benghaner@users.noreply.github.com>
2022-07-03 16:03:50 -0400

    Update home and search to get trails from API (#8)
    * Create the request and endpoint
    
    * Organize files
    
    * Create the handler
    
    * Update home page to load from API
    
    * Fix edit trail endpoint missing attribute
    
    * Fix null reference exception
    
    * Use placeholder image
    
    * Add link to edit trail
    
    * Fix alert displays in error on edit

---
+++ BlazingTrails.Shared/Features/Home/Shared/GetTrailsRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BlazingTrails.Shared.Features.Home.Shared;
+
+public class GetTrailsRequest : IRequest<GetTrailsRequest.Response>
+{
+    public const string RouteTemplate = "/api/trails";
+
+    public record Trail(int Id, string Name, string Location, int TimeInMinutes, int Length, string Description);
+
+    public record Response(IEnumerable<Trail> Trails);
+}
